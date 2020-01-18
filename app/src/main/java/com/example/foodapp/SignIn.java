8507f9ebc4c51846ef08c0eb6f4fb2cc5d5ed159
Patrick Louis <patricklouisw@gmail.com>
2020-01-18 16:57:38 -0500

    Changes in Sign

--- app/src/main/java/com/example/foodapp/SignIn.java
+++ app/src/main/java/com/example/foodapp/SignIn.java
@@ -2,13 +2,31 @@ package com.example.foodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignIn extends AppCompatActivity {
+public class SignIn extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogIn;
+
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        btnLogIn = findViewById(R.id.btnLogIn);
+        btnSignUp = findViewById(R.id.btnSignUp);
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
