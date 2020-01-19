c49a36801ec18d0cc3697a2e751f19f902383afa
Patrick Louis <patricklouisw@gmail.com>
2020-01-18 19:56:46 -0500

    Attempting on Impact (Google Maps).
    Create a new activity (About)

--- app/src/main/java/com/example/foodapp/MainPage.java
+++ app/src/main/java/com/example/foodapp/MainPage.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.widget.ImageButton;
 
-public class MainPage extends AppCompatActivity {
+public class MainPage extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
     }
 
     public void toPoints(View view) {
@@ -36,6 +39,5 @@ public class MainPage extends AppCompatActivity {
         Intent i = new Intent(MainPage.this, MainActivity.class);
         startActivity(i);
     }
-
 
 }
